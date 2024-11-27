333fbb48d61fd4adf678db58ad0e8dfa3b541726
Davidek523 <davidov520@wp.pl>
2024-11-27 23:43:42 +0100

    Changes
    Adding:
    - Textures to the rects
    - Background color

--- code/sprite.py
+++ code/sprite.py
@@ -1,9 +1,9 @@
 from settings import *
 
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self, pos, surf, groups):
+    def __init__(self, pos, surf = pygame.Surface((16, 16)), groups = None):
         super().__init__(groups)
-        self.image = pygame.Surface((16, 16))
-        self.image.fill("White")
+        self.image = surf
+        # self.image.fill("White")
         self.rect = self.image.get_frect(topleft=pos)
         self.old_rect = self.rect.copy()
